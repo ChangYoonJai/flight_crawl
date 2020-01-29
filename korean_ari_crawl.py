ffc833ae7eb69df068ecddf85637cafeb8629b7f
Yoonjai Chang <ChangYoonJai@gmail.com>
2020-01-30 00:25:06 +0900

    Update

--- korean_ari_crawl.py
+++ korean_ari_crawl.py
@@ -1,7 +1,49 @@
-import requests 
-from ylog import ylog
-log = ylog()
+#-*- coding:utf-8 -*-
+# import ylog as log
+from selenium import webdriver
+from bs4 import BeautifulSoup
 
-page = requests.get("http://dataquestio.github.io/web-scraping-pages/simple.html") 
-log.d(page.content)
+# setup Driver|Chrome : 크롬드라이버를 사용하는 driver 생성
+driver = webdriver.Chrome('chromedriver')
+def wait(delay=1):
+    driver.implicitly_wait(delay)
+wait(1)
+driver.get('https://www.koreanair.com/mobile/korea/ko/ibe/bookingGate.html?isDomestic=false')
+wait(1)
+html = driver.page_source # 페이지의 elements모두 가져오기
+soup = BeautifulSoup(html, 'html.parser') # BeautifulSoup사용하기
 
+            # <option selected="selected" value="roundtrip">
+            #  왕복
+            # </option>
+            # <option value="oneway">
+            #  편도
+            # </option>
+            # <option value="multimore">
+            #  다구간 여정
+            # </option>
+
+tripTypeMenu = driver.find_element_by_name('tripTypeMenu') #.send_keys('multimore')
+tripTypeMenu:selenium.webdriver.remote.webelemen
+print(type(tripTypeMenu), tripTypeMenu)
+tripTypeMenu.select('multimore')
+
+
+with open('test.txt', mode='w', encoding='utf8') as fp:
+    fp.write(soup.prettify())
+
+
+# # Login
+# driver.get('https://nid.naver.com/nidlogin.login') # 네이버 로그인 URL로 이동하기
+# driver.find_element_by_name('id').send_keys('changyoonjai') # 값 입력
+# driver.find_element_by_name('pw').send_keys('ynvn*4*1')
+# driver.find_element_by_xpath(
+#     '//*[@id="frmNIDLogin"]/fieldset/input'
+# ).click() # 버튼클릭하기
+# driver.get('https://order.pay.naver.com/home') # Naver 페이 들어가기
+# html = driver.page_source # 페이지의 elements모두 가져오기
+# soup = BeautifulSoup(html, 'html.parser') # BeautifulSoup사용하기
+# notices = soup.select('div.p_inr > div.p_info > a > span')
+
+
+print(input('DONE'))
--- ylog.py
+++ ylog.py
@@ -1,19 +1,92 @@
+import os, inspect
 import logging
+import logging.handlers
+import pandas as pd
 
-class ylog(logging.Logger):
 
-    def __init__(self, name='default', level=logging.DEBUG):
-        super().__init__(name, level)
-        self.d = self.debug
-        FORMAT =  "[%(levelname)-8s][%(filename)15s:%(lineno)-4s][%(funcName)20s()] %(message)s"
-        logging.basicConfig(level=logging.DEBUG,
-                            format=FORMAT,
-                            datefmt='%m-%d %H:%M',
-                            filename=f'{name}.log',
-                            filemode='w')
-        console = logging.StreamHandler()
-        console.setLevel(logging.DEBUG)
-        formatter = logging.Formatter(FORMAT)
-        console.setFormatter(formatter)
-        logging.getLogger('').addHandler(console)
+# formatter & file logging
+FORMAT = "[%(levelname)-8s][%(filename)s:#%(lineno)4s] %(funcName)s() > %(message)s"
+logging.basicConfig(level=logging.DEBUG,
+                    format=FORMAT,
+                    datefmt='%m-%d %H:%M',
+                    filename='log.log',
+                    filemode='w')
+console = logging.StreamHandler()
+console.setLevel(logging.DEBUG)
+formatter = logging.Formatter(FORMAT)
+console.setFormatter(formatter)
+__logger = logging.getLogger('')
+__logger.addHandler(console)
+
+d = logging.debug
+i = logging.info
+w = logging.warning
+e = logging.error
+c = logging.critical
+
+
+
+def k(value):
+    caller = inspect.getframeinfo(inspect.stack()[1][0])
+    levelname = 'DEBUG'
+    filename = os.path.basename(caller.filename)
+    lineno = caller.lineno
+    funcName = caller.function
+    message = f'{type(value)} - {value}'
+    print(f'[{levelname:8}][{filename}:#{lineno:4}] {funcName}() > {message}')
+
+def p(message):
+    caller = inspect.getframeinfo(inspect.stack()[1][0])
+    levelname = 'DEBUG'
+    filename = os.path.basename(caller.filename)
+    lineno = caller.lineno
+    funcName = caller.function
+    print(f'[{levelname:8}][{filename}:#{lineno:4}] {funcName}() > ...')
+    print(message)
+
+# class log:
+
+#     __logger = logging.getLogger('')
+#     tag = ''
+
+#     @classmethod
+#     def tag_by_main_script(cls, script_file):
+#         cls.tag = os.path.splitext(os.path.basename(script_file))[0]
     
+#     my_format = '[{levelname:8}][{filename}:#{lineno:4}] {funcName}() > {message}'
+
+#     @classmethod
+#     def k(cls, value):
+#         caller = inspect.getframeinfo(inspect.stack()[1][0])
+#         levelname = 'DEBUG'
+#         filename = os.path.basename(caller.filename)
+#         lineno = caller.lineno
+#         funcName = caller.function
+#         # FORMAT = "[%(levelname)-8s][%(filename)s:#%(lineno)4s] %(funcName)s() > %(message)s"
+#         message = f'{type(value)} - {value}'
+#         print(f'[{levelname:8}][{filename}:#{lineno:4}] {funcName}() > {message}')
+
+#     @classmethod
+#     def d(cls, message):
+#         cls.__logger.debug(f'[{cls.tag}] {message}')
+
+#     @classmethod
+#     def i(cls, message):
+#         # caller = inspect.getframeinfo(inspect.stack()[1][0])
+#         # levelname = 'DEBUG'
+#         # filename = os.path.basename(caller.filename)
+#         # lineno = caller.lineno
+#         # funcName = caller.function
+#         cls.__logger.info('[' + cls.tag + '] ' + message)
+
+#     @classmethod
+#     def w(cls, message):
+#         cls.__logger.warning('[' + cls.tag + '] ' + message)
+
+#     @classmethod
+#     def e(cls, message):
+#         cls__logger.error('[' + cls.tag + '] ' + message)
+
+#     @classmethod
+#     def c(cls, message):
+#         cls.__logger.critical('[' + cls.tag + '] ' + message)
